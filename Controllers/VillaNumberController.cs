983feda6809e72677e8f3ab8f910e5c4edbdec28
schneidermatic <markus.schneider73@gmail.com>
2023-06-18 15:21:54 +0200

    * Updated - 81/Display API Error Messages.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   Controllers/VillaNumberController.cs

--- Controllers/VillaNumberController.cs
+++ Controllers/VillaNumberController.cs
@@ -67,6 +67,14 @@ namespace MagicVilla_Web.Controllers
                 {
                     return RedirectToAction(nameof(IndexVillaNumber));
                 }
+                else
+                {
+                    if(response.ErrorMessages.Count > 0)
+                    {
+                        ModelState.AddModelError("ErrorMessages", response.ErrorMessages.FirstOrDefault());
+                    }
+                }
+
             }
 
             var resp = await _villaService.GetAllAsync<APIResponse>();
